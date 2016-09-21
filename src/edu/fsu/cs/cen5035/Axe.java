627af88656405c49feed1b1666b0903edc2380e7
xinlingxinyu <xinlingxinyu@DESKTOP-QLD3PQR>
2016-09-21 13:38:38 -0400

    Add Axe.java file
    This file re-implement Axe class

---
+++ src/edu/fsu/cs/cen5035/Axe.java
@@ -0,0 +1,31 @@
+package edu.fsu.cs.cen5035;
+
+/**
+ *
+ * @author Yuchuan
+ */
+public class Axe extends BasicWeapon implements Weapon {
+
+    public Axe() {
+        super(60);
+    }
+
+    @Override
+    public int hit() {
+        return DAMAGE;
+    }
+
+    @Override
+    public int hit(int armor) {
+	int damage;
+	if(armor > 0 && armor < 20)
+            damage = DAMAGE;
+	else 
+	    damage = DAMAGE - armor;
+
+        if (damage < 0) {
+            return 0;
+        }
+        return damage;
+    }
+}
