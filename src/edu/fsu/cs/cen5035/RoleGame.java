c30ab8bb9c2630961bd119c41ae1fe39d6a16a08
xinlingxinyu <xinlingxinyu@DESKTOP-QLD3PQR>
2016-09-21 13:51:17 -0400

    Update RoleGame.java and WeaponFactory.java files
    These files are updated to include all weapons and test the result

--- src/edu/fsu/cs/cen5035/RoleGame.java
+++ src/edu/fsu/cs/cen5035/RoleGame.java
@@ -18,6 +18,20 @@ public class RoleGame {
         System.out.println("Swords was able to do " + sword.hit(armor) + " of damage due to an armor with "+armor+ " points.");
         
         //TODO: Add the remainig weapons here
+        Weapon arrow = WeaponFactory.getWeapon("arrow");
+        System.out.println("Arrow has " + arrow.hit() + " of damage.");
+        armor = 20;
+        System.out.println("Arrow was able to do " + arrow.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+	    Weapon axe = WeaponFactory.getWeapon("axe");
+        System.out.println("Axe has " + axe.hit() + " of damage.");
+        armor = 20;
+        System.out.println("Axe was able to do " + axe.hit(armor) + " of damage due to an armor with "+armor+ " points.");
+
+	    Weapon magicstaff = WeaponFactory.getWeapon("magicstaff");
+        System.out.println("MagicStaff has " + magicstaff.hit() + " of damage.");
+        armor = 20;
+        System.out.println("MagicStaff was able to do " + magicstaff.hit(armor) + " of damage due to an armor with "+armor+ " points.");
 
     }
 }
